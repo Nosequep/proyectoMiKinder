ef43e1e81364faf12092ec699b6d4b406b7251c5
Joel Valenzuela <56770129+feeljoe@users.noreply.github.com>
2021-04-14 02:04:04 -0700

    Agregado de pantallas
    Se agregaron las pantallas de perfil, recursos, agenda y trofeos

---
+++ app/src/main/java/valera/jesus/proyectomikinder/TrofeosActivity.kt
@@ -0,0 +1,11 @@
+package valera.jesus.proyectomikinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrofeosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trofeos)
+    }
+}
