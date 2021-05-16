fb985f9c87785a1e05e4b7f97a4287bf71d76ea1
Jesús Abraham Valera Valdez <poupapitas86@gmail.com>
2021-05-16 15:09:13 -0700

    Agregando login

--- app/src/main/java/valera/jesus/proyectomikinder/MainActivity.kt
+++ app/src/main/java/valera/jesus/proyectomikinder/MainActivity.kt
@@ -4,19 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
         var b_aceptar: Button = findViewById(R.id.b_aceptar)
 
         b_aceptar.setOnClickListener {
-            var intent = Intent(this, InicioActivity::class.java)
-            startActivity(intent)
+            this.valida_ingreso()
         }
 
         var b_registrar: Button = findViewById(R.id.b_registrarse)
@@ -24,5 +30,40 @@ class MainActivity : AppCompatActivity() {
             var intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun valida_ingreso(){
+
+
+        var correo: String = this.et_correo.text.toString()
+        var contra: String = this.et_contrasenia.text.toString()
+
+        if(!correo.isNullOrBlank() && !contra.isNullOrBlank()){
+            ingresaFirebase(correo, contra)
+        }else{
+            Toast.makeText(this, "Ingresar datos",
+                    Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun ingresaFirebase(email:String, password:String){
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+
+                        val user = auth.currentUser
+                        var intent = Intent(this, InicioActivity::class.java)
+                        startActivity(intent)
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+
+
+                        Toast.makeText(baseContext, "La contraseña o el correo son incorrectas.",
+                                Toast.LENGTH_SHORT).show()
+
+                    }
+                }
     }
 }
--- app/src/main/java/valera/jesus/proyectomikinder/RegisterActivity.kt
+++ app/src/main/java/valera/jesus/proyectomikinder/RegisterActivity.kt
@@ -2,10 +2,91 @@ package valera.jesus.proyectomikinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var storage: FirebaseFirestore
+    private lateinit var usuario : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = Firebase.auth
+        //Conexion a la bd
+        storage = FirebaseFirestore.getInstance()
+        //conexion al usuario gmail
+        usuario = FirebaseAuth.getInstance()
+        this.b_registrarse.setOnClickListener{
+            valida_registro()
+        }
+
+    }
+
+    private fun valida_registro(){
+        var nombre: String = et_nombre.text.toString()
+        var apellido: String = et_apellido.text.toString()
+        var correo: String = et_correo.text.toString()
+        var contra1: String = et_contrasenia1.text.toString()
+        var contra2: String = et_contrasenia2.text.toString()
+
+        if(!nombre.isNullOrBlank() && !apellido.isNullOrBlank() && !contra1.isNullOrBlank()){
+            if(contra1 == contra2){
+                registrarFirebase(correo, contra1)
+            }else{
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(this, "Favor de llenar todos los campos.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun registrarFirebase(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+
+                    val user = auth.currentUser
+                    registrarUsuario()
+
+
+
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+
+    private fun registrarUsuario(){
+        val usuario = hashMapOf(
+            "nombres" to et_nombre.text.toString(),
+            "apellidos" to et_apellido.text.toString(),
+            "correo" to et_correo.text.toString(),
+            "contrasenia" to et_contrasenia1.text.toString(),
+            "rol" to "alumno"
+        )
+        storage.collection("usuario")
+            .add(usuario)
+            .addOnSuccessListener {
+                Toast.makeText(baseContext, "Se ha registrado satisfactoriamente.",
+                    Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(baseContext, "Error en el registro.",
+                    Toast.LENGTH_SHORT).show()
+            }
     }
 }
